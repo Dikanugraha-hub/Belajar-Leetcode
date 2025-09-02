1d644032aebc764f4ecc1aa77903079fca6c20ef
Dikanugraha-hub <pramudyan196@gmail.com>
2025-09-02 22:27:37 +0700

    Time: 0 ms (100.00%) | Memory: 8.1 MB (58.93%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.c
+++ 14-longest-common-prefix/longest-common-prefix.c
@@ -1,19 +1,22 @@
 char* longestCommonPrefix(char** strs, int strsSize) {
-    if (strsSize == 0) return "";
+    if(strsSize == 0) {
+        return "";
+    }
+   
+    char* prefix = (char*)malloc(strlen(strs[0]) + 1);
+    strcpy(prefix, strs[0]);
 
-    // Ambil string pertama sebagai prefix awal
-    char* prefix = strs[0];
-
-    for (int i = 1; i < strsSize; i++) {
-        // Cek huruf per huruf antara prefix dengan strs[i]
+    for(int i = 1; i < strsSize; i++) {
         int j = 0;
         while (prefix[j] && strs[i][j] && prefix[j] == strs[i][j]) {
             j++;
         }
-        // Potong prefix sesuai panjang common prefix saat ini
+
         prefix[j] = '\0';
 
-        if (j == 0) return ""; // Tidak ada common prefix
+        if(j == 0) {
+            return "";
+        }
     }
 
     return prefix;
